b49d97e594ec90beea71f70227127f4f354df0d8
Lulu Tung <lulutheflaneur@gmail.com>
2023-10-13 10:28:19 -0400

    Align items on cause cards

--- src/Components/CauseCard.tsx
+++ src/Components/CauseCard.tsx
@@ -4,11 +4,11 @@ import HELP_ICON from '../imgs/help-icon.png'
 export const CauseCard = ({cause}) => {
 
     return (
-        <div id="card" className="p-2 border rounded-xl px-5 border-none h-[200px] bg-teal-100 hover:bg-teal-200">
+        <div id="card" className="flex items-center p-2 border rounded-xl px-5 border-none h-[200px] bg-white justify-items-center hover:bg-teal-100">
             <Link to={`/charity/${cause.name}`}>
-                <div className="grid grid-cols-3">
+                <div className="grid grid-cols-3 ">
                     <img className="w-10 h-10" src={cause.logoUrl ?? HELP_ICON}/>
-                    <span className="col-span-2">{cause.name}</span>
+                    <span className="opacity-100 col-span-2">{cause.name}</span>
                     {/*<br/>*/}
                     {/*<span>{cause.location}</span>*/}
                 </div>
