ad143a38405456da38914d8acf1d00d482f604ca
Lulu Tung <lulutheflaneur@gmail.com>
2023-10-12 22:43:45 -0400

    Fix vercel rerouting

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import {useEffect, useMemo, useState} from "react";
 import {CauseCard} from "./Components/CauseCard.tsx";
 import CAUSE_LIST from "../CauseList.json";
 import {browseCause} from "./axios.ts";
+import {IBrowseCharity} from "./types/charity.tsx";
 
 function App() {
 
     const [shouldFadeIn, setShouldFadeIn] = useState(false)
-    const [causeList, setCauseList] = useState([])
+    const [causeList, setCauseList] = useState<IBrowseCharity[]>([])
     const randomCause = useMemo(() => CAUSE_LIST[Math.floor(Math.random() * CAUSE_LIST.length)], [])
-
 
     useEffect( () => {
         setShouldFadeIn(true)
@@ -19,21 +19,6 @@ function App() {
         browseCause({cause: randomCause, setCauseList})
     }, [randomCause])
 
-
-    // const fetchCharity = async () => {
-    //     try {
-    //         const res = await axios.get<{
-    //             nonprofits: IBrowseCharity[]
-    //         }>(`https://partners.every.org/v0.2/browse/${cause}?apiKey=${import.meta.env.VITE_API_KEY}`)
-    //         console.log('res', res)
-    //         setResList(res.data.nonprofits)
-    //
-    //     } catch (e) {
-    //         console.log('e', e)
-    //     }
-    // }
-
-
     return (
         <>
             <div className={`bg-cover bg-center w-full h-160 transition-all duration-1000 ${shouldFadeIn ? 'opacity-100' : 'opacity-25'}`} style={{
@@ -41,7 +26,6 @@ function App() {
             }}>
                 <Navbar />
             </div>
-            {/*<img className="object-cover h-10 w-full" src="" />*/}
             <div>Find a charity you may be interested in...</div>
             <div className="grid grid-cols-5 gap-3">
                 {causeList.map((cause) =>
