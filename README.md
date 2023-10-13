6a59b5d809d06fca17bb268a26e36186ad84677e
Lulu Tung <lulutheflaneur@gmail.com>
2023-10-12 23:47:14 -0400

    Add default img to search page

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 ## Attribution
 <a href="https://www.flaticon.com/free-icons/favourite" title="favourite icons">Favourite icons created by Smashicons - Flaticon</a>  
+[Charity picture](https://unsplash.com/photos/A4Ax1ApccfA?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash)
--- src/Pages/search-page.tsx
+++ src/Pages/search-page.tsx
@@ -18,7 +18,7 @@ export const SearchPage = () => {
     console.log('causeList', causeList[0])
 
     return (<>
-            <CoverImg imgUrl={causeList?.[0]?.coverImageUrl ?? ''}>
+            <CoverImg imgUrl={causeList?.[0]?.coverImageUrl ?? 'https://images.unsplash.com/photo-1593113598332-cd288d649433?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80'}>
                 <Navbar/>
             </CoverImg>
             <h1>Search results for: {cause}</h1>
